d27e3ee135f26dfa4cc5979e1a77a8d80845567d
laushinka <laurie.malau@gmail.com>
2016-06-26 16:27:43 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Laurie."
+  end
+
+  get '/hometown' do
+    "My hometown is Bogor, Indonesia."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Vienna by Billy Joel."
+  end
 end
